452b03ccba2e8042e2543fec3be69a9bc78b28b8
Juan Barrera <jcbp1999@gmail.com>
2024-09-22 16:35:39 +1000

    basic linked list constructor

---
+++ linked_list/basic_constructor.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+example_linked_list = LinkedList(1)
+print(example_linked_list.head.value)
