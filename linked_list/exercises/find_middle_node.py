9123eaab16b87d7b980f4c4c067315f3a1bccb49
Juan Barrera <jcbp1999@gmail.com>
2024-09-22 19:53:43 +1000

    linked list exercise find middle node method

---
+++ linked_list/exercises/find_middle_node.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+
+    def find_middle_node(self):
+        slow_pointer = self.head
+        fast_pointer = self.head
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+        return slow_pointer
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+
+
+print(my_linked_list.find_middle_node().value)
