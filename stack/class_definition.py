ed020208babe3c02f658a8fc558c569456a00ff1
Juan Barrera <jcbp1999@gmail.com>
2024-10-06 22:33:10 +1100

    push method added to the stack DS class

--- stack/class_definition.py
+++ stack/class_definition.py
@@ -15,3 +15,13 @@ class Stack:
         while current_node:
             print(current_node.value)
             current_node = current_node.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
