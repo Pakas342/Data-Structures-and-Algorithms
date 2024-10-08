b4eff4b2fceaa7e1bcf6301f5fab8aba18eb5bfc
Juan Barrera <jcbp1999@gmail.com>
2024-10-08 23:17:41 +1100

    class definition and constructor for the queue DS

---
+++ queue/class_definition.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        current_node = self.first
+        while current_node:
+            print(current_node.value)
+            current_node = current_node.next
+
