33e7e1e649bd501ffb3beba0a2e679b0eb8e5834
Juan Barrera <jcbp1999@gmail.com>
2024-10-10 18:20:53 +1100

    push method on list stack exercise solved

--- stack/exercises/push_method_list_stack.py
+++ stack/exercises/push_method_list_stack.py
@@ -6,8 +6,5 @@ class Stack:
         for i in range(len(self.stack_list) - 1, -1, -1):
             print(self.stack_list[i])
 
-    # WRITE PUSH METHOD HERE #
-    #                        #
-    #                        #
-    #                        #
-    ##########################
+    def push(self, value):
+        self.stack_list.append(value)
