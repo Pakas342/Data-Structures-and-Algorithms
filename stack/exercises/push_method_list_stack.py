4b8f1234a0952b143b86ddd67d6c0070eb2eac5f
Juan Barrera <jcbp1999@gmail.com>
2024-10-10 18:19:57 +1100

    push method on list stack exercise

---
+++ stack/exercises/push_method_list_stack.py
@@ -0,0 +1,13 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list) - 1, -1, -1):
+            print(self.stack_list[i])
+
+    # WRITE PUSH METHOD HERE #
+    #                        #
+    #                        #
+    #                        #
+    ##########################
